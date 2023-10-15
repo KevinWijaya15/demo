5a76a0eac24e153ed41e79fdcc007e2417c02797
Kevin Wijaya <81453326+KevinWijaya15@users.noreply.github.com>
2023-10-14 18:04:19 -0700

    Add files via upload

---
+++ sidenav.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+			
+	$(".sidenav-toggler").on('click', function() {
+
+		$(".nav").toggleClass("active");
+		$(".sidenav-toggler").toggleClass("active");
+
+	});
+
+});
